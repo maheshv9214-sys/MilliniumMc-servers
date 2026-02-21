30773a1abe415e90e4962f86a2529d9c5898f94b
maheshv9214-sys <mahesh.v9214@gmail.com>
2026-02-21 22:40:17 +0530

    Create vps-config.js with VPS configuration class

---
+++ vps-config.js
@@ -0,0 +1,19 @@
+class VPSConfiguration {
+    connectToVPS() {
+        // Code to connect to the VPS
+    }
+
+    installMinecraftServer() {
+        // Code to install the Minecraft server
+    }
+
+    configureFirewall() {
+        // Code to configure the firewall
+    }
+
+    setupServerProperties() {
+        // Code to set up server properties
+    }
+}
+
+module.exports = VPSConfiguration;
